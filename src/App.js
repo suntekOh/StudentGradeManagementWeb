8fde59a1bda5d1ac9a979464be8027ebf5103e2b
Suntek Oh <suntekoh@outlook.com>
2023-01-31 22:01:39 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./css/site.css"
+import { Tabs, Tab, Row, Button, Col, Container, Stack } from "react-bootstrap";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="container-fluid px-0">
+            <div class="d-flex flex-column vh-100">
+                <div class="fixed-top d-flex custom-bg-gray flex-wrap custom-header-bg">
+                    <div class="p-3">Flex item</div>
+                    <div class="p-3">Flex item</div>
+                    <div class="p-3">Flex item</div>
+                </div>
+                <div class="d-flex flex-grow-1 flex-wrap content-padding-top">
+                    <div class="custom-bg-gray vh-100 overflow-auto sidebar-fixed-width">
+                        <div> Flex items1</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items1</div>
+                    </div>
+                    <div class="p-2 flex-grow-1 overflow-auto custom-bg-gray flex-wrap vh-100">
+                        <div> Flex items1</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items1</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+
+                        <div> Flex items1</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items</div>
+                        <div> Flex items1</div>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-wrap custom-footer-bg">
+                    <div class="p-3 bd-custom-gray">Flex item</div>
+                    <div class="p-3 bd-custom-gray">Flex item</div>
+                    <div class="p-3 bd-custom-gray">Flex item</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
+
